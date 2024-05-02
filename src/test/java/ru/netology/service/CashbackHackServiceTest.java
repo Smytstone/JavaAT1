4e5e105e644bc54496efc313224f6a5f216facdd
Alexandr Shchelokov <alex175171@gmail.com>
2024-05-02 14:58:42 +0300

    testing branch added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void ShouldRemainTestCase1() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(900), 100);
+    }
+
+    @Test
+    public void ShouldRemainTestCase2() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(999), 1);
+    }
+
+    @Test
+    public void ShouldRemainTestCase3() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(1_000), 0);
+    }
+
+    @Test
+    public void ShouldRemainTestCase4() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(1_001), 999);
+    }
+}
